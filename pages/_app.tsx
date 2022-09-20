e28ffc8c7a18618c9b23717cd694d0a7c3d11ab4
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-09-21 03:10:08 +0530

    modify ChakraUI default theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+import {ChakraProvider, extendTheme} from "@chakra-ui/react"
+// reset css for every single browser
+import 'reset-css'
+
+// ChakraUI has a weird bluish-grey
+// customizing that to a decent color
+const theme = extendTheme({
+  colors : {
+    gray: {
+      100: '#F5f5f5',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+
+  // fixing anchor tag, and their underline and shadow
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none'
+          }
+        }
+      }
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
