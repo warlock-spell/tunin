6e1eb8da284250dc4facc19913d2a773004eb50d
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-09-21 05:06:21 +0530

    import dependencies for sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,27 @@
+// Importing list elements, as side bar has mainly items displayed in list format
+// Divider is to divide various section in sidebar, inspiration from spotify itself
+// LinkBox and LinkOverlay gives us a click target (large click area around a small word)
+import {
+    Box,
+    List,
+    ListItem,
+    ListIcon,
+    Divider,
+    Center,
+    LinkBox,
+    LinkOverlay
+} from '@chakra-ui/layout'
+
+// Importing react icons -> https://react-icons.github.io/react-icons/
+// Again all imports are according to an inspiration from spotify playlist side bar
+import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite
+} from 'react-icons/md'
+
 const Sidebar = () => {
     return <div>hello from sidebar</div>
 }
