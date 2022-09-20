6d8b1867b024354856370e8f88fbb4d660f477f8
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-09-21 04:40:09 +0530

    playlist container and playbar setup

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -5,9 +5,23 @@ import {Box} from '@chakra-ui/layout'
 
 const PlayerLayout = ({children}) => {
     return (
-        <Box>
-            Layout
-            {children}
+        // vw - view width, vh = view height -> responsive unit of measurement
+        // This will prevent main page scrolling, instead each individual thing scrolls on its own 
+        // you can use sx parameter to add custom css
+        <Box width="100vw" height='100vh'>
+            {/* avoid using % in playlist bar width, as it will look weird on ultrawide screens */}
+            <Box position='absolute' top='0' left='0' width='250px'>
+              sidebar
+            </Box>
+            <Box marginLeft='250px' marginBottom='100px'>
+                {/* left margin so that it aligns right next to side bar */}
+                {/* bottom margin so that it aligns right above the player bar */}
+                {children}
+            </Box>
+            <Box position= 'absolute' left='0' bottom='0'>
+                player
+            </Box>
+
         </Box>
     )
 }
