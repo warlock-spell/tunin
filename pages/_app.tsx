0c50ffa1fea66b6f02f839b264dfd324ab95ae33
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-10-16 18:50:02 +0530

    auth pages opted out of player layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,9 +43,14 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <PlayerLayout>
+      {Component.authPage ? (
+        // If the component has an authPage property, do not wrap it in layout
         <Component {...pageProps} />
-      </PlayerLayout>
+      ): <PlayerLayout>
+          <Component {...pageProps} />
+         </PlayerLayout>
+      }
+      
     </ChakraProvider>
   )
 }
