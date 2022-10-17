c32f1f0747244215904f86962d2ae383bec46aa2
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-10-18 03:49:31 +0530

    add avatar in rounded box

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,15 @@ import GradientLayout from '../components/gradientLayout'
 
 
 const Home = () => {
-  return (<GradientLayout color="green" subTitle="profile" title="Daksh Gaur" description="15 public playlists">
+  return (
+  <GradientLayout
+   roundImage
+   color="green" 
+   subTitle="profile" 
+   title="Daksh Gaur" 
+   description="15 public playlists"
+   image="/jin.jpg"
+   >
     <div>Volaa</div>
   </GradientLayout>)
 }
