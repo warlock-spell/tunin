f1a3c344a9f4f3a519332a82a30397c9e2779d53
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2022-10-25 21:59:08 +0530

    gradient layout for playlist pages

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,10 +1,32 @@
 // a new route created /playlist/id
 
+import GradientLayout from "../../components/gradientLayout"
 import { validateToken } from "../../lib/auth"
 import prisma from "../../lib/prisma"
 
+const getBGColor = id => {
+    const colors = [
+        'red',
+        'green',
+        'blue',
+        'yellow',
+        'teal',
+        'purple',
+        'orange',
+        'gray'
+
+    ]
+
+    return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
+}
+
 const Playlist = ({playlist}) => {
-    return <div>{playlist.name}</div>
+    const color = getBGColor(playlist.id)
+    return (
+        <GradientLayout color={color}>
+            <div>Content here</div>
+        </GradientLayout>
+    )
 }
 
 export const getServerSideProps = async ({query, req}) => {
